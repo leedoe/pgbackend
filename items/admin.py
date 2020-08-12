139fc610eccb2d1f4305873b29548b9bfd34568a
DOJUN <leedoe0709@gmail.com>
2020-08-12 09:57:57 +0900

    item admin추가

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,8 @@
+from items.models import Item
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "store_name"]
+    search_fields = ["name"]
