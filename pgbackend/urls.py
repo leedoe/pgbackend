5a1d74cc15ee1ffc6d250ed4286790fed8e033a3
DOJUN <leedoe0709@gmail.com>
2020-08-12 21:05:22 +0900

    필요없는 import 삭제

--- pgbackend/urls.py
+++ pgbackend/urls.py
@@ -17,9 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from users.api.views import UserViewSet
