b5946806cb163eee04312860dc93a667a0d95511
DOJUN <leedoe0709@gmail.com>
2020-08-30 00:37:41 +0900

    가죽 정보에 관련된 모델 추가

--- items/models.py
+++ items/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+from stores.models import Store
 
 
 class Item(models.Model):
@@ -8,3 +11,35 @@ class Item(models.Model):
     link = models.URLField(max_length=300)
     soldout = models.BooleanField()
     store_name = models.CharField(max_length=191)
+
+
+class Tannery(models.Model):
+    name = models.CharField(max_length=150)
+    nationality = models.CharField(max_length=100)
+    explanation = models.TextField()
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    explanation = models.TextField()
+
+
+class Leather(models.Model):
+    TANNING_METHOD = [
+        (0, 'Vegetable'),
+        (1, 'Chrome'),
+        (2, 'Hybrid')
+    ]
+
+    name = models.CharField(max_length=150)
+    tanery = models.ForeignKey(Tannery, on_delete=models.CASCADE)
+    tanning_method = models.IntegerField(choices=TANNING_METHOD)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    explanation = models.TextField()
+
+
+class LeatherDetail(models.Model):
+    leather = models.ForeignKey(Leather, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    note = models.TextField()
